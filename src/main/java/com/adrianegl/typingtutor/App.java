36d1a52190519e9945c87e5d995d47ce9b5de389
Adrians29 <adrian.egl@hotmail.com>
2026-09-13 19:31:54 -0400

    set a stage for display

--- src/main/java/com/adrianegl/typingtutor/App.java
+++ src/main/java/com/adrianegl/typingtutor/App.java
@@ -3,6 +3,7 @@ package com.adrianegl.typingtutor;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,7 +15,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 1200, 720);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
