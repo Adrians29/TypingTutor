8b17cfb91a2978da216ef20450fc28213428a179
Adrians29 <adrian.egl@hotmail.com>
2026-09-19 11:51:50 -0400

    verify diplay of the keybord on scene

--- src/main/java/com/adrianegl/typingtutor/App.java
+++ src/main/java/com/adrianegl/typingtutor/App.java
@@ -1,10 +1,12 @@
 package com.adrianegl.typingtutor;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -16,17 +18,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label label = new Label("Press a key");
-        GridPane keyboardGrid = new GridPane();
-        keyboardGrid.setHgap(5);
-        keyboardGrid.setVgap(5);
-        Scene scene = new Scene(keyboardGrid, 1200, 720);
-        keyboardGrid.getChildren().add(label);
+        Keyboard keyboard = new Keyboard();
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.add(keyboard, 0, 0);
+        //Label label = new Label("Press a key");
+        //keyboardGrid.setHgap(5);
+        //keyboardGrid.setVgap(5);
+        Scene scene = new Scene(gridPane, 1200, 720);
+        /*keyboardGrid.getChildren().add(label);
         scene.setOnKeyPressed(e -> {
            KeyCode keyCode = e.getCode();
            String keyText = e.getText();
            label.setText(keyText);
-        });
+        });*/
         stage.setScene(scene);
         stage.show();
     }
