f9bb20a0fc616e8edb4422d27fc7a3cf244e3c07
Adrians29 <adrian.egl@hotmail.com>
2026-09-19 13:50:02 -0400

    chang the KeyEvent to recognize the letter of the sentece and define if the key press is right or wrong

--- src/main/java/com/adrianegl/typingtutor/App.java
+++ src/main/java/com/adrianegl/typingtutor/App.java
@@ -1,6 +1,7 @@
 package com.adrianegl.typingtutor;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -18,16 +19,31 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        Keyboard keyboard = new Keyboard();
+        
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-        gridPane.add(keyboard, 0, 0);
+        Keyboard keyboard = new Keyboard();
+        Label label = new Label("Try typing this text. Do it as quickly and accurately as you can.");
+        
+        gridPane.add(keyboard, 0, 7);
+        gridPane.add(label, 0, 6);
         //Label label = new Label("Press a key");
         //keyboardGrid.setHgap(5);
         //keyboardGrid.setVgap(5);
+        
         Scene scene = new Scene(gridPane, 1200, 720);
         scene.addEventFilter(KeyEvent.KEY_PRESSED, e -> {
-            keyboard.highLight(e.getCode());
+            int i = 0;
+            char charKey = e.getText().charAt(0);
+            String str = "Try typing this text. Do it as quickly and accurately as you can.";
+            if (charKey == str.toUpperCase().charAt(i)) {
+                keyboard.highLight(e.getCode());
+                i++;
+            } else if (charKey == str.toUpperCase().charAt(i)) {
+                keyboard.highLight(e.getCode());
+                i++;
+            }
+
         });
         scene.addEventFilter(KeyEvent.KEY_RELEASED, e -> {
             keyboard.unHighLight(e.getCode());
@@ -38,6 +54,7 @@ public class App extends Application {
            String keyText = e.getText();
            label.setText(keyText);
         });*/
+        
         stage.setScene(scene);
         stage.show();
     }
@@ -45,5 +62,23 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    
+    /*public String nextPhrase(int num) {
+        String str = switch(num) {
+            case 1 -> "Try typing this text. Do it as quickly and accurately as you can.";
+            case 2 -> "Next type another line of input data.";
+            case 3 -> "The quick brown fox jumps over the lazy dog.";
+            case 4 -> "Five big quacking zephyrs jolt my wax bed.";
+            case 5 -> "Sympathizing would fix Quaker objectives.";
+            case 6 -> "A large fawn jumped quickly over white zinc boxes.";
+        };
+        
+        char c = ' ';
+        for (int i = 0; i < str.length(); i++) {
+            str.charAt(i);
+        }
+        
+        return str;
+    }*/
 
 }
