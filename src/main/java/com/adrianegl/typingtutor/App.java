09971fc0033bc159d8b28c00d8b7cd4f3e264d2f
Adrians29 <adrian.egl@hotmail.com>
2026-09-23 14:11:17 -0400

    create a method for the display of the mistake count

--- src/main/java/com/adrianegl/typingtutor/App.java
+++ src/main/java/com/adrianegl/typingtutor/App.java
@@ -22,6 +22,7 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
     private int currentIdx = 0;
+    private int currentMistake = 0;
     private int mistake = 0;
     private int i = 1;
     private String str = nextPhrase(i);
@@ -31,7 +32,7 @@ public class App extends Application {
         strContainer = new TextFlow();
         strContainer.getStyleClass().add("sentence-container");
         
-        SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
+        SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
         
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
@@ -40,7 +41,10 @@ public class App extends Application {
         bRest.setText("RESET");
         Button bNext = new Button();
         bNext.setText("NEXT");
-        Label lb = new Label("");
+        
+        Label lbCount = new Label(wordCount(mistake, str.length()));
+        
+        lbCount.setStyle("-fx-background-color: gray; -fx-text-fill: black; -fx-font-weight: bold; -fx-background-radius: 5;");
         
         bRest.getStyleClass().add("control-button");
         bRest.getStyleClass().add("reset-button");
@@ -50,7 +54,7 @@ public class App extends Application {
         bRest.setFocusTraversable(false);
         
         gridPane.add(keyboard, 0, 7);
-        gridPane.add(lb, 0, 6);
+        gridPane.add(lbCount, 0, 4);
         gridPane.add(strContainer, 0, 5);
         gridPane.add(bRest, 0, 8);
         gridPane.add(bNext, 1, 8);
@@ -71,12 +75,12 @@ public class App extends Application {
                     if (upperCharKey == str.charAt(currentIdx)) {
                         keyboard.highlightRight(e.getCode());
                         currentIdx++;
-                        SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
+                        SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
                     } else if (upperCharKey != str.toUpperCase().charAt(currentIdx)) {
                         keyboard.highlightWrong(e.getCode());
-                        ++mistake;
-                        SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
-                        --mistake;
+                        ++currentMistake;
+                        SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
+                        --currentMistake;
                     }
                 }
             } else if (e.isShiftDown() && str.toUpperCase().charAt(currentIdx) != str.charAt(currentIdx)){
@@ -87,12 +91,12 @@ public class App extends Application {
                 if (charKey == str.charAt(currentIdx)) {
                     keyboard.highlightRight(e.getCode());
                     currentIdx++;
-                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
+                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
                 } else if (charKey != str.toUpperCase().charAt(currentIdx)) {
                     keyboard.highlightWrong(e.getCode());
-                    ++mistake;
-                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
-                    --mistake;
+                    ++currentMistake;
+                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
+                    --currentMistake;
                 }
             } else{
                 KeyCode code = e.getCode();
@@ -102,12 +106,13 @@ public class App extends Application {
                 if (charKey == str.charAt(currentIdx)) {
                     keyboard.highlightRight(e.getCode());
                     currentIdx++;
-                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
+                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
                 } else if (charKey != str.toUpperCase().charAt(currentIdx)) {
                     keyboard.highlightWrong(e.getCode());
+                    ++currentMistake;
                     ++mistake;
-                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
-                    --mistake;
+                    SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
+                    --currentMistake;
                 }
                 if (currentIdx == str.length()) {
                     bNext.setDisable(false);
@@ -126,7 +131,7 @@ public class App extends Application {
         bRest.addEventHandler(MouseEvent.MOUSE_CLICKED,e -> {
             if (e.getButton() == MouseButton.PRIMARY) {
                 currentIdx = 0;
-                mistake = 0;
+                currentMistake = 0;
             }
         });
         
@@ -134,9 +139,9 @@ public class App extends Application {
             ++i;
             str = nextPhrase(i);
             strContainer.getStyleClass().add("sentence-container");
-            mistake = 0;
+            currentMistake = 0;
             currentIdx = 0;
-            SentenceDisplay.updateSentence(strContainer, str, currentIdx, mistake);
+            SentenceDisplay.updateSentence(strContainer, str, currentIdx, currentMistake);
             bNext.setDisable(true);
         });
         
@@ -166,6 +171,10 @@ public class App extends Application {
             default:
                 return "Try typing this text. Do it as quickly and accurately as you can.";
         }
+    }
+    
+    public static String wordCount(int mistake, int length) {
+        return String.format("%d/%d", mistake, length);
     }
     
 }
